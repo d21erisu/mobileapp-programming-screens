b9221b7cbc9556741c192aa4bc2d71d6ed712c45
Erik <d21erisu@student.his.se>
2022-04-28 08:33:49 +0200

    added button that starts second activity and displays data

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -2,7 +2,9 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("string");
+
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(name);
+
+
+
+        }
     }
 }
